be1b78444b660c703dc0e155324f269ffa6f5320
kbc18a11 <wada121400@gmail.com>
2020-12-10 10:54:05 +0900

    終わった

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const data = await (await axios.get('https://zipcloud.ibsnet.co.jp/api/search?zipcode=7900014')).data;
+  res.jsonp({ data: data });
 });
 
 module.exports = router;
