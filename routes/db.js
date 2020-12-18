5427840900518548b34a4d2d56abc4260d11f854
kbc18a11 <wada121400@gmail.com>
2020-12-18 12:06:20 +0900

    PostgreSQL Test

---
+++ routes/db.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../pool');
+
+router.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
